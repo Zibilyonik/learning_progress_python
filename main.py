e1dd0634db5520c82e7ae3ac900695d68487b2c6
Onur Surmeli <onatsurmeli@gmail.com>
2023-05-23 20:10:54 +0200

    added regexes for name, surname email, added proper outputs

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 #Python3
-
+import re
 def student_check(data):
-    pass
+    if data == '':
+        print("Incorrect credentials")
+        return False
+    name_regex = r"^[A-Za-z][A-Za-z\-\']*[A-Za-z]$"
+    surname_regex = r"^[A-Za-z][A-Za-z\-\']*[A-Za-z]$"
+    email_regex = r"[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*"
+    name = data.split()[0]
+    surname = data.split()[1:-1]
+    email = data.split()[-1]
+    if len(data.split()) < 3:
+        print("Incorrect credentials")
+        return False
+    if not re.match(name_regex, name):
+        print("Incorrect first name")
+        return False
+    if not all(re.match(surname_regex, s) for s in surname):
+        print("Incorrect surname")
+        return False
+    if not re.match(email_regex, email):
+        print("Incorrect email")
+        return False
+    return True
 
 
 def student_add():
+    count = 0
     print("Enter student credentials or 'back' to return:")
     user_input = input().strip()
-    if user_input == "back":
-        return False
-    if student_check(user_input):
-        return True
+    while user_input != "back":
+        if student_check(user_input):
+            print("Student has been added.")
+            count += 1
+        user_input = input().strip()
+    return count
 
 
 def command_list(command):
@@ -18,18 +42,21 @@ def command_list(command):
         if command == "exit":
             print('Bye!')
             return
-        elif not command:
+        if not command:
             print('No input')
         elif command == "add students":
-            count = 0
-            while student_add():
-                count += 1
-            print(f'Total {count} students have been added.')
+            print(f'Total {student_add()} students have been added.')
+        elif command == "back":
+            print("Enter 'exit' to exit the program")
         else:
             print('Unknown command!')
+        command = input()
 
 
 def main():
     print("Learning progress tracker")
     command = input()
     command_list(command.strip())
+
+if __name__ == '__main__':
+    main()
